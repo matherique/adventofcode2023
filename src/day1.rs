a12c4629309253b0c244b25076b95fe9a9f1d4b8
Matheus Henrique <matherique@gmail.com>
2023-12-01 11:32:28 -0300

    feat: added solution to day 1 part 2

--- src/day1.rs
+++ src/day1.rs
@@ -12,25 +12,40 @@ treb7uchet      77
 fn main() -> Result<()> {
     let content = utils::read_file("1")?;
 
-    // let letters = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let letters = [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    let options = [
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "zero", "one", "two", "three", "four",
+        "five", "six", "seven", "eight", "nine",
+    ];
 
     let all_digits: Vec<usize> = content
         .trim_end()
         .split("\n")
         .map(|line| {
-            let numbers: String = line.chars().filter(|c| c.is_numeric()).collect();
+            let mut digits: Vec<usize> = Vec::new();
 
-            let mut digits = String::from("");
+            for i in 0..line.len() {
+                if let Some(parts) = line.get(i..line.len()) {
+                    for (j, letter) in options.iter().enumerate() {
+                        if parts.starts_with(letter) {
+                            if j >= 10 {
+                                digits.push(j - 10);
+                            } else {
+                                digits.push(j)
+                            }
 
-            if let Some(first) = numbers.chars().next() {
-                digits.push(first);
+                            println!("{letter} {parts}");
+                            break;
+                        }
+                    }
+                }
             }
 
-            if let Some(last) = numbers.chars().last() {
-                digits.push(last);
-            }
-
-            digits.parse::<usize>().unwrap_or_default()
+            let (first, last) = (digits.first().unwrap_or(&0), digits.last().unwrap_or(&0));
+            return (first * 10) + last;
         })
         .collect();
 
